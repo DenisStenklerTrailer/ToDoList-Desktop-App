4bab28ec77b7453962bc6813007420e79765c44c
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-01 11:36:45 +0100

    Graphics changes
    Small graphics changes such as background and add button image background

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("DarkTeal1")
+
+clock = sg.Text("",key="clock")
 title = sg.Text('To-Do App')
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=3, image_source="add.png", tooltip="Add to-do", key="Add")
 # showing the todos
 list_box = sg.Listbox(values=functions.ReadingTodos(), key="todos", enable_events=True, size=[45,10])
 
@@ -13,11 +17,13 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[title], [input_box, add_button],[list_box, edit_button, complete_button], [exit_button]],
-                   element_justification="center", font=('Helvetica', 15)) # Layout mora bit list
+                   layout=[[clock],[title], [input_box, add_button],[list_box, edit_button, complete_button], [exit_button]],
+                   font=('Helvetica', 15)) # Layout mora bit list
 
 while True:
-    event, values = window.read() #Displays the window actually on the screen
+    event, values = window.read(timeout=10) #Displays the window actually on the screen, Izvaja se na vsake 10ms.
+
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     #print(1, event)
     #print(2, values)
     #print(3, values['todos'])
@@ -28,25 +34,31 @@ while True:
         functions.WritingTodos(todos)
         window["todos"].update(values=todos)  #todos je tukaj key ki ga definiramo zgoraj ko kreiramo objekt
     elif event == "Edit":
-        todo_to_edit = values["todos"][0]
+        try:
+            todo_to_edit = values["todos"][0]
 
-        new_todo = values["todo"] + "\n"
+            new_todo = values["todo"] + "\n"
 
-        todos = functions.ReadingTodos()
+            todos = functions.ReadingTodos()
 
-        index = todos.index(todo_to_edit)
-        todos[index] = new_todo
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
 
-        functions.WritingTodos(todos)
-        window["todos"].update(values=todos) #updejtanje seznama ko kliknemo edit
+            functions.WritingTodos(todos)
+            window["todos"].update(values=todos) #updejtanje seznama ko kliknemo edit
+        except IndexError:
+            sg.popup("Please select an item first!", font=("Helvetica", 16))
     elif event == "Complete":
-        todo_to_complete = values["todos"][0]
-        todos = functions.ReadingTodos()
-        todos.remove(todo_to_complete)
+        try:
+            todo_to_complete = values["todos"][0]
+            todos = functions.ReadingTodos()
+            todos.remove(todo_to_complete)
 
-        functions.WritingTodos(todos)
-        window["todos"].update(values=todos)
-        window["todo"].update("")
+            functions.WritingTodos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update("")
+        except IndexError:
+            sg.popup("Please select an item first!", font=("Helvetica", 16))
     elif event == "Exit":
         break
     elif event == "todos": #ta event je na todos ko kliknemo na enega izmed to do-jev v seznamu
